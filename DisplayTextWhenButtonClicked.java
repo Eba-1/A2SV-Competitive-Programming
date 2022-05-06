1671f82d2000956b289ab11dfeb7b1d50464dd40
Eba-1 <95696690+Eba-1@users.noreply.github.com>
2022-05-06 10:30:52 +0530

    TextView

--- DisplayTextWhenButtonClicked.java
+++ DisplayTextWhenButtonClicked.java
@@ -28,6 +28,28 @@ public void method_name() {
       Log.view_name(tag:"button", msg:"Button Clicked");      
 } 
 
+
+// For the editext view (a view where the user can input data)
+// Declare a edit text view at the top
+TextView name;
+// Then inside the onCreate method, write this code
+name = findViewById(button_id); 
+button_name = findViewById(button_id); 
+
+// Now create an onclick listener method right below it
+button_name.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+            method_name(); // calls the method
+      }
+});
+
+// Add this method
+public void method_name() {
+      Log.view_name(tag:"some_tag_for_button", msg:"Button Clicked");  
+      Log.view_name(tag:"some_tag_for_editText", msg:"User input: " + name.getText().toString());
+}
+
 // To see the content in the log, go down to the debugger panel and open the logcat
 // Type in the tag you specified inside the search button and click verbose
 // Now the text will be displayed whenever the event you specified occurs
