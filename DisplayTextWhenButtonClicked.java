bae4d4458caf98e7d17fe2a68a1a618a3796e74d
Eba-1 <95696690+Eba-1@users.noreply.github.com>
2022-05-06 09:47:47 +0530

    Update DisplayTextWhenButtonClicked.java

--- DisplayTextWhenButtonClicked.java
+++ DisplayTextWhenButtonClicked.java
@@ -1,6 +1,8 @@
+// This little program displays a message on the log whenever the user clicks the button.
 // First of all, you need to create a button and give it an id. Inside the MainActivity.java file write the below code:
 
 public class_name(View view_name) {
-      v = findViewById(button_id);
+//       v = findViewById(button_id); Not needed
       
+      Log.view_name(tag:"button", msg:"Button Clicked");      
 }  
