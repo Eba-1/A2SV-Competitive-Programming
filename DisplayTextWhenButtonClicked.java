0056017be043bd6a7a33d6a48440c0317fde777e
Eba-1 <95696690+Eba-1@users.noreply.github.com>
2022-05-06 09:40:03 +0530

    Create DisplayTextWhenButtonClicked.java

---
+++ DisplayTextWhenButtonClicked.java
@@ -0,0 +1,6 @@
+// First of all, you need to create a button and give it an id. Inside the MainActivity.java file write the below code:
+
+public class_name(View view_name) {
+      v = findViewById(button_id);
+      
+}  
