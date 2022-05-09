3ec0cae0e585936ad64a2353ddd77c6815c062ff
Eba-1 <95696690+Eba-1@users.noreply.github.com>
2022-05-09 17:57:06 +0530

    Create Kilo->Pound.java

---
+++ Kilo->Pound.java
@@ -0,0 +1,2 @@
+// A simple program that will convert a number that a user enter into its pound equivalent
+// Create two text fields and a button. Style them as per your wish and write this code inside the MainActivity.java file.
